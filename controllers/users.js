a9a927479eae7cea13c47d82a2598163ed2bc3f1
Tien Pham <r3dsky02@yahoo.com.vn>
2019-02-02 22:51:18 +0200

    Login route

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const User = require('../models/users');
 
 const connUri = process.env.MONGO_LOCAL_CONN_URL;
+const bcrypt = require('bcrypt');
 
 module.exports = {
   add: (req, res) => {
@@ -31,4 +32,45 @@ module.exports = {
       }
     });
   },
+  login: (req, res) => {
+    const { name, password } = req.body;
+
+    mongoose.connect(connUri, { useNewUrlParser: true }, (err) => {
+      let result = {};
+      let status = 200;
+      if(!err) {
+        User.findOne({name}, (err, user) => {
+          if (!err && user) {
+            // We could compare passwords in our model instead of below
+            bcrypt.compare(password, user.password).then(match => {
+              if (match) {
+                result.status = status;
+                result.result = user;
+              } else {
+                status = 401;
+                result.status = status;
+                result.error = 'Authentication error';
+              }
+              res.status(status).send(result);
+            }).catch(err => {
+              status = 500;
+              result.status = status;
+              result.error = err;
+              res.status(status).send(result);
+            });
+          } else {
+            status = 404;
+            result.status = status;
+            result.error = err;
+            res.status(status).send(result);
+          }
+        });
+      } else {
+        status = 500;
+        result.status = status;
+        result.error = err;
+        res.status(status).send(result);
+      }
+    });
+  }
 }
