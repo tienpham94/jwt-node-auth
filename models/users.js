652343b1ebb1b3af21e7d9f178ddbf8fc11f2c18
Tien Pham <r3dsky02@yahoo.com.vn>
2019-02-02 22:48:42 +0200

    Encrypt

--- models/users.js
+++ models/users.js
@@ -21,4 +21,22 @@ const userSchema = new Schema({
   }
 });
 
+// encrypt password before save
+userSchema.pre('save', function(next) {
+  const user = this;
+  if(!user.isModified || !user.isNew) { // don't rehash if it's an old user
+    next();
+  } else {
+    bcrypt.hash(user.password, stage.saltingRounds, function(err, hash) {
+      if (err) {
+        console.log('Error hashing password for user', user.name);
+        next(err);
+      } else {
+        user.password = hash;
+        next();
+      }
+    });
+  }
+});
+
 module.exports = mongoose.model('User', userSchema);
